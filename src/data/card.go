98adc52d242d152aa4fe3f5385ee8e20dc8c093e
eaglerayp <b98705002@gmail.com>
2017-12-28 21:02:08 +0800

    update: v1

--- src/data/card.go
+++ src/data/card.go
@@ -6,22 +6,26 @@ import (
 )
 
 type Activity struct {
-	Id            bson.ObjectId `bson:"_id"`
-	Card          string        `bson:"Card"`
-	DiscountType  string        `bson:"DiscountType"`
-	ActivityName  string        `bson:"ActivityName"`
-	Content       string        `bson:"Content"`
-	Remark        string        `json:"-" bson:"Remark"`
-	StoreName     string        `json:"StoreName,omitempty" bson:"StoreName"`
-	Branch        string        `json:"Branch,omitempty" bson:"Branch"`
-	Address       string        `json:"Address,omitempty" bson:"Address"`
-	Tel           string        `json:"Tel,omitempty" bson:"Tel"`
-	GoogleAdrress string        `json:"GoogleAdrress,omitempty" bson:"GoogleAdrress"`
-	Time          string        `bson:"Time"`
-	Longutitude   float64       `json:"Longutitude,omitempty" bson:"Longutitude"`
-	Latitude      float64       `json:"Latitude,omitempty" bson:"Latitude"`
-	IsToday       bool          `bson:"-"`
+	Id              bson.ObjectId `bson:"_id"`
+	Card            string        `bson:"Card"`
+	DiscountType    string        `bson:"DiscountType"`
+	ActivityName    string        `bson:"ActivityName"`
+	Content         string        `bson:"Content"`
+	Remark          string        `json:"-" bson:"Remark"`
+	StoreBranchName string        `json:"StoreBranchName,omitempty" bson:"StoreBranchName"`
+	Address         string        `json:"Address,omitempty" bson:"Address"`
+	Tel             string        `json:"Tel,omitempty" bson:"Tel"`
+	GoogleAddress   string        `json:"GoogleAddress,omitempty" bson:"GoogleAddress"`
+	Time            string        `bson:"Time"`
+	Longutitude     float64       `json:"Longutitude,omitempty" bson:"Longutitude"`
+	Latitude        float64       `json:"Latitude,omitempty" bson:"Latitude"`
+	IsToday         bool          `bson:"-"`
 }
+
+const (
+	DB         = "esun"
+	Collection = "Activity"
+)
 
 var (
 	mongoSession *mgo.Session
@@ -32,10 +36,24 @@ func InitMongo(url string) (err error) {
 	return
 }
 
+func GetJob() (results []Activity, err error) {
+	results = []Activity{}
+	// Card == card AND Discount == discount
+	selector := bson.M{"$or": []bson.M{bson.M{"GoogleAddress": bson.M{"$exists": false}}, bson.M{"GoogleAddress": nil}}}
+	err = mongoSession.DB(DB).C(Collection).Find(selector).All(&results)
+	return
+}
+
+func Set(doc Activity) error {
+	selector := bson.M{"_id": doc.Id}
+	_, err := mongoSession.DB(DB).C(Collection).Upsert(selector, doc)
+	return err
+}
+
 func GetActivity(card, discount []string) (results []Activity, err error) {
 	results = []Activity{}
 	// Card == card AND Discount == discount
 	selector := bson.M{"Card": bson.M{"$in": card}, "DiscountType": bson.M{"$in": discount}}
-	err = mongoSession.DB("esun").C("Activity").Find(selector).All(&results)
+	err = mongoSession.DB(DB).C(Collection).Find(selector).All(&results)
 	return
 }
--- src/rest/route.go
+++ src/rest/route.go
@@ -1,16 +1,19 @@
 package rest
 
 import (
+	"context"
 	"data"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"googlemaps.github.io/maps"
 )
 
 type Input struct {
@@ -20,6 +23,7 @@ type Input struct {
 type Response struct {
 	Message string      `json:"Message,omitempty"`
 	Result  interface{} `json:"Result,omitempty"`
+	Count   int         `json:"Count,omitempty"`
 }
 
 func InitHackAPI() *gin.Engine {
@@ -28,6 +32,44 @@ func InitHackAPI() *gin.Engine {
 	router.POST("/echo", func(c *gin.Context) {
 		inputByte, _ := ioutil.ReadAll(c.Request.Body)
 		c.String(http.StatusOK, string(inputByte))
+	})
+
+	router.GET("/googlemapjob", func(c *gin.Context) {
+		// init google map client
+		gClient, err := maps.NewClient(maps.WithAPIKey("AIzaSyCOupeguSkCc8RDiLTkA94uk02Jzpq5xSo"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, Response{Message: err.Error()})
+			return
+		}
+		ctx := context.Background()
+		//get job
+		jobs, err := data.GetJob()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, Response{Message: err.Error()})
+			return
+		}
+		// fmt.Println("jobs:", jobs)
+		for _, job := range jobs {
+			// ask google
+			resp, err := gClient.Geocode(ctx, &maps.GeocodingRequest{Address: job.Address, Language: "zh-TW"})
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			// fmt.Printf("%+v\n", resp)
+			if resp == nil || len(resp) == 0 {
+				log.Println("No data:", job.Id)
+				continue
+			}
+			gData := resp[0]
+			job.GoogleAddress = gData.FormattedAddress
+			job.Latitude = gData.Geometry.Location.Lat
+			job.Longutitude = gData.Geometry.Location.Lng
+			// set
+			data.Set(job)
+		}
+
+		c.JSON(http.StatusOK, Response{Count: len(jobs), Result: "OK"})
 	})
 
 	// input as /activity?input={"Card":["幸運PLUS鈦金卡"],"DiscountType":["食"]}
